8e1f020d010e2894ad772779bc6c7cde4f9f14fc
kiran-930 <159869124+kiran-930@users.noreply.github.com>
2024-11-12 10:57:31 +0530

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -4,6 +4,6 @@ const connection_string = process.env.CONNECTION_STRING;
 
 mongoose.connect(connection_string).then((res)=>{console.log("Mongodb Atlas connected with pfServer");
 }).catch((err)=>{
-    console.log("Connection failed");
+    console.log("Connection failed.");
     console.log(err);
 })
